f683585d067354a242cb736194265b1c5ea5dbd4
sanjeev <sanjeev.bitx@gmail.com>
2023-12-20 13:13:17 +0530

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,66 @@
+import { Heading, IconButton, VStack, useColorMode } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
+import { useEffect, useState } from "react";
 function App() {
+  const initialTodos = [
+    {
+      id: 1,
+      body: "get bread",
+    },
+    {
+      id: 2,
+      body: "get butter",
+    },
+    {
+      id: 3,
+      body: "get jam",
+    },
+  ];
+
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem("todos")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function deleteTodo(id) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  function addTodo(todo) {
+    setTodos([...todos, todo]);
+  }
+
+  const {colorMode, toggleColorMode} = useColorMode()
   return (
     <>
-      <h1>App</h1>
+      <VStack p={4}>
+        <IconButton
+          icon={colorMode==='light'?<FaSun />:<FaMoon/>}
+          isRound="true"
+          size="lg"
+          alignSelf="flex-end"
+          onClick={toggleColorMode}
+        />
+        <Heading
+          mb="8"
+          fontWeight="extrabold"
+          size="2xl"
+          bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+          bgClip="text"
+        >
+          Todo Application
+        </Heading>
+        <TodoList todos={todos} deleteTodo={deleteTodo} />
+        <AddTodo addTodo={addTodo} />
+      </VStack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode='light'/>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
